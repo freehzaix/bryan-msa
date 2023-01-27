056468c60f2bc1be4dbd82b83010cf5ff983b328
freehzaix <freehzaix@gmail.com>
2023-01-27 19:57:53 +0000

    Sauvegarde du 27_01_2023

--- resources/views/espace-membre.blade.php
+++ resources/views/espace-membre.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Espace membre - MSA</title>
+    <title>MSA</title>
 
     <!-- Font Awesome -->
 
@@ -16,8 +15,8 @@
     <!-- Main css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="/frontend/Assets/css/memb.css">
-
+    <link rel="stylesheet" href="../Assets/css/memb.css">
+    <script src="https://kit.fontawesome.com/f47a995387.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -28,7 +27,7 @@
         </div>
     </nav>
 
-    <nav class="navbar navbar-expand-lg  fixed-top" style="background-color: #0d8ae2;">
+    <nav class="navbar navbar-expand-lg  fixed-top " style="background-color: #0d8ae2;">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">MSA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
@@ -36,14 +35,15 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Bienvenue</a>
+                        <a class="nav-link active text-white text-capitalize fw-semibold" aria-current="page"
+                            href="#">Bienvenue</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            {{ session('membre')->prenom }} {{ session('membre')->nom }}
+                        <a class="nav-link dropdown-toggle text-white text-capitalize fst-italic" href="#"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-house-user"></i> {{ session('membre')->prenom }} {{ session('membre')->nom }}
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/logout">Deconnection</a></li>
@@ -57,28 +57,38 @@
     </nav>
 
 
-    <div class="row row-cols-1 row-cols-md-4 g-4">
+    <div class="container-fluid">
+        <div class="row mt-4 ">
 
-        @foreach ($membres as $membre)
+            @foreach ($membres as $membre)
 
-        <div class="col">
-            <div class="card">
-                <img src="/storage/images/{{ $membre->image }}" class="card-img-top img-thumbnail" alt="{{ $membre->image }}">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $membre->nom }} {{ $membre->prenom }}</h5>
-                    <p class="card-text">
-                        {{ $membre->telephone }} <br />
-                        {{ $membre->quartier }} <br />
-                        {{ $membre->colonne }} <br />
-                        {{ $membre->departement }} <br />
-                        <a href="mailto:{{ $membre->email }}">{{ $membre->email }}</a>
-                    </p>
+            <div class="col-md-4 ">
+                <div class="card mb-3 shadow p-3 mb-5 bg-body rounded border-0" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-5">
+                            <img src="/storage/images/{{ $membre->image }}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card-body font-roboto ">
+                                <h5 class="card-title fw-bold text-warning">{{ $membre->nom }} {{ $membre->prenom }}</h5>
+                                <p class="card-text text-capitalize fw-semibold"> <i
+                                        class="fa-solid fa-building-user"></i> Colone : {{ $membre->colonne }} </p>
+                                <p class="card-text text-capitalize fw-semibold"> <i
+                                        class="fa-solid fa-building-user"></i> Departement : {{ $membre->departement }}</p>
+                                <p class="card-text fw-semibold"> <i class="fa-solid fa-mobile-screen-button"></i>
+                                    {{ $membre->telephone }}</p>
+                                <p class="card-text text-capitalize fst-italic"><small class="text-muted"><i
+                                            class="fa-solid fa-map-location-dot"></i> Habite {{ $membre->quartier }}</small></p>
+                                            <a href="mailto:{{ $membre->email }}">{{ $membre->email }}</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            @endforeach
+
         </div>
-
-        @endforeach
-
     </div>
 
 
